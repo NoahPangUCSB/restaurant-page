2e63ce8751c60d9698eef232a9caaca0d702fe72
Noah Pang <noahpang@cs.ucsb.edu>
2023-09-02 16:40:43 -0700

    added tab button animations

--- src/header.js
+++ src/header.js
@@ -7,14 +7,17 @@ export default function header() {
 
   const homeBtn = document.createElement("button");
   homeBtn.classList.add("home-btn");
+  homeBtn.classList.add("selected-tab");
   homeBtn.textContent = "Home";
 
   const menuBtn = document.createElement("button");
   menuBtn.classList.add("menu-btn");
+  menuBtn.classList.add("unselected-tab");
   menuBtn.textContent = "Menu";
 
   const contactBtn = document.createElement("button");
   contactBtn.classList.add("contact-btn");
+  contactBtn.classList.add("unselected-tab");
   contactBtn.textContent = "Contact";
 
   tabWrapper.appendChild(homeBtn);
@@ -25,3 +28,52 @@ export default function header() {
 
   return headerContent;
 }
+
+export const displayController = (() => {
+    let tabButtons = null;
+    const _getTabs = () => {
+        tabButtons = document.querySelectorAll("button");
+    }
+
+    const _removeSelectedTabClass = () => {
+        tabButtons.forEach(element => {
+            element.classList.remove("selected-tab");
+            element.classList.add("unselected-tab");
+        });
+    }
+
+    const _removeHoveredTabClass = (e) => {
+        e.srcElement.classList.remove("hovered-tab");
+    }
+
+    const displaySelectedTab = () => {
+        if(!tabButtons) {
+            _getTabs();
+        }
+
+        tabButtons.forEach(element => {
+            element.addEventListener("click", () => {
+                _removeSelectedTabClass();
+                element.classList.add("selected-tab");
+            })
+        });
+    }
+
+    const displayHoveredTab = () => {
+        if(!tabButtons) {
+            _getTabs();
+        }
+
+        tabButtons.forEach(element => {
+            element.addEventListener("mouseover", () => {
+                element.classList.add("hovered-tab");
+            })
+        });
+
+        tabButtons.forEach(element => {
+            element.addEventListener("mouseout", _removeHoveredTabClass);
+        });
+    }
+    
+    return { displaySelectedTab, displayHoveredTab };
+})();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import displayHome from "./home";
 import header from "./header";
 import footer from "./footer";
+import { displayController as headerDisplay } from "./header";
 import './css/style.css';
 
 function displayHeader() {
     const headerContent = header();
+    
 
     return headerContent;
 }
@@ -27,6 +29,14 @@ function displayFooter() {
     return footerContent;
 }
 
+const displayController = () => {
+    
+    headerDisplay.displaySelectedTab();
+    headerDisplay.displayHoveredTab();
+};
+
 document.body.appendChild(displayHeader());
 document.body.appendChild(displayContent());
 document.body.appendChild(displayFooter());
+
+displayController();
